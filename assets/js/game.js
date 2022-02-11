73c81c52d749fef0a316e535d045a1bc824af924
superfishal <97133716+superfishal@users.noreply.github.com>
2022-02-10 18:33:47 -0800

    player name null bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -167,11 +167,19 @@ var shop = function() {
 
 /* END GAME FUNCTIONS */
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
 /* GAME INFORMATION / VARIABLES */
-
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
